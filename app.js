e382853f05e57c427ca87d6477d08b2baef47e4b
me <culestfrosh@gmail.com>
2022-08-26 20:22:21 +0100

    added socket

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ var logger = require('morgan');
 
 var app = express();
 
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -86,7 +91,11 @@ app.get("/songs", (req, res)=> {
     songs: musicArray
   })
 })
-app.get("/", (req, res)=>res.sendFile(/*path.join(__dirname,*/ "/Templates/index.html"))
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+app.get("/", (req, res)=>res.sendFile(__dirname, "/Templates/index.html"))
 //
 //
 // catch 404 and forward to error handler
