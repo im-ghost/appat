da445db584fde94a121fb5d5c1fdbea18781226e
me <culestfrosh@gmail.com>
2022-08-27 15:03:52 +0100

    Added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Templates/*",
+    "./public",
+    "./public/**/*.{html,js}",
+    "./public/**/*.{html,js}",
+    "./Templates/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
