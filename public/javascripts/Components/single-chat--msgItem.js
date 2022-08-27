e9b57cf77ee9717e5797ff3e6b9ad27187cfd590
me <culestfrosh@gmail.com>
2022-08-27 09:16:36 +0100

    created msg list component
    structuring the application like react

---
+++ public/javascripts/Components/single-chat--msgItem.js
@@ -0,0 +1,39 @@
+const singleMsg = () => {
+  const template = document.createElement("template")
+  template.innerHTML = `
+  <style>
+  </style>
+  <div>
+  <div id="msg"></div>
+  <div id="time"></div>
+  </div>
+
+  `
+
+
+  class SingleChatMsgItem extends HTMLElement {
+    constructor() {
+      super();
+      this.attachShadow({
+        mode: "open"
+      });
+      this.shadowRoot.appendChild(template.content.cloneNode(true))
+      const dom = this.shadowRoot;
+      const sel = (el) => {
+        return dom.querySelector(el)
+      }
+      this.shadowRoot.querySelector("#msg").innerHTML = `${this.getAttribute("text")}`
+      this.shadowRoot.querySelector("#time").innerHTML = `${this.getAttribute("time")}`
+    }
+    connectedCallback() {
+      this.shadowRoot.querySelector("#r").addEventListener("click", ()=>this.toggle());
+    }
+    disconnectedCallback() {
+      this.shadowRoot.querySelector("#r").removeEventListener()
+    }
+  }
+
+
+  window.customElements.define("chat-msg", SingleChatMsgItem);
+}
+export default singleMsg
