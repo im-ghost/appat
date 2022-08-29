33cb33c67f78c8a2c72c2f7fc8ae4d5256370d17
me <culestfrosh@gmail.com>
2022-08-29 11:10:44 +0100

    Turned route to ts

---
+++ routes/index.ts
@@ -0,0 +1,16 @@
+var express = require('express');
+
+import {
+  Request,
+  Response,
+  NextFunction
+} from "express"
+
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req: Request, res:Response, next:NextFunction) {
+  res.render('index', { title: 'Appat' });
+});
+
+module.exports = router;
