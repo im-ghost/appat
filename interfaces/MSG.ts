e0b426962355bbeb757277123bef641eebeaeea3
me <culestfrosh@gmail.com>
2022-08-29 23:15:26 +0100

    updated msg interface

--- interfaces/MSG.ts
+++ interfaces/MSG.ts
@@ -4,8 +4,8 @@ import ROOM from "./ROOM"
 import USER from "./USER"
 export interface MSG {
   text: string,
-  room: ROOM,
-  sender: USER
+  sender: USER,
+  time: Date,
 }
 
 export type MSGS = MSG[]
