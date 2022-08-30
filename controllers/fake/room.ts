82fc2416224155be47d829817cafc0154f4c1e4e
me <culestfrosh@gmail.com>
2022-08-30 23:04:04 +0100

    updated fake room controller

--- controllers/fake/room.ts
+++ controllers/fake/room.ts
@@ -2,7 +2,8 @@
 var fs = require("fs");
 var path = require("path")
 var filepath = path.resolve(__dirname, "../../fakedata.json")
-const readFile = async(file: string)=> {
+//Read
+const readFile = async(file: string = filepath )=> {
   try {
     const res = await fs.readFileSync(path.resolve(__dirname, file), "utf8")
 
@@ -17,6 +18,8 @@ const cota = async () => {
   let dddr = await readFile("../../fakedata.json")
   return dddr
 }
+
+//Create 
 const addRoom = async(body: any)=> {
   cota()
   .then(async (dat)=> {
@@ -35,14 +38,54 @@ const addRoom = async(body: any)=> {
       })
   })
 }
-addRoom({
-  name: "me",
-  _id: 300,
-  members: [],
-  messages: []
-})
+
+//Update 
+
+const UpdateRoom = async(id:any,body: any)=> {
+  cota()
+  .then(async (dat)=> {
+    
+  
+   let dd =  dat.rooms.map((room:any)=>{
+      if(room._id === id){
+        room = {...body}
+      }
+    })
+    await fs.writeFile(filepath,
+      JSON.stringify(dd),
+      (err: any)=> {
+        if (err) {
+          console.log(err);
+          return err
+        }
+        console.log("saved")
+
+      })
+  })
+}
+
+const deleteRoom = async(id:any)=> {
+  cota()
+  .then(async (dat)=> {
+   let dd =  dat.rooms.filter((room:any)=>room._id === id)
+    await fs.writeFile(filepath,
+      JSON.stringify(dd),
+      (err: any)=> {
+        if (err) {
+          console.log(err);
+          return err
+        }
+        console.log("saved")
+
+      })
+  })
+}
+
 module.exports = {
-  readFile
+  readFile,
+  addRoom,
+  UpdateRoom,
+  deleteRoom
 }
 //
 //
