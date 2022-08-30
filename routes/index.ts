9c6feaab2d709969df74df16a98f14c005525797
me <culestfrosh@gmail.com>
2022-08-30 23:03:14 +0100

    updated routes index

--- routes/index.ts
+++ routes/index.ts
@@ -9,8 +9,13 @@ import {
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req: Request, res:Response, next:NextFunction) {
-  res.render('index', { title: 'Appat' });
+router.get('/', function(req: Request, res: Response, next: NextFunction) {
+  setTimeout(()=> {
+    console.log("a"+req.app.get("socket"))
+  }, 6000)
+  res.render('index', {
+    title: 'Appat'
+  });
 });
 
 module.exports = router;
