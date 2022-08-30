7c2c195cce5faf04d9870c9fd64eb85219fb5c24
me <culestfrosh@gmail.com>
2022-08-30 22:58:06 +0100

    made the room routes return fake data

--- routes/rooms.ts
+++ routes/rooms.ts
@@ -7,17 +7,45 @@ import {
   Response,
   NextFunction
 } from "express"
-
+const {
+  readFile
+} = require("../controllers/fake/room")
 const router = Router();
 
-router.get("/", (req: Request, res:Response, next:NextFunction)=> {
+router.get("/", (req: Request, res: Response, next: NextFunction)=> {
+  let socket = req.app.get("socket")
+  let io = req.app.get("io")
+  if (socket) {
+    console.log("client didn't make a request oo")
+    socket.on("getRooms", async ()=> {
+      readFile()
+      .then((rooms: any)=> {
+        io.emit("allRooms", rooms.rooms)
+      })
+      console.log("all rooms if")
+    })
+  } else {
+    console.log("else")
+    io.on("connection", async(socket: any)=> {
+      console.log("h")
+
+      socket.on("getRooms", async ()=> {
+        console.log("trying to get all rooma")
+
+        readFile()
+        .then((rooms: any)=> {
+          io.emit("allRooms", rooms.rooms)
+        })
+      })
+    })
+  }
   res.render("rooms", {
     title: "All rooms"
   })
 })
 
 
-router.get("/:id", (req: Request, res: Response, next:NextFunction)=> {
+router.get("/:id", (req: Request, res: Response, next: NextFunction)=> {
   res.render("room", {
     title: `Room`
   })
