f63ebb4ff634ca7f783aca7da46e191d1ecce1f1
me <culestfrosh@gmail.com>
2022-08-31 22:52:12 +0100

    added pug watching

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./Templates/*",
     "./public",
+    "./views",
+    "./views/*.pug",
+    "./views/**/*.pug",
     "./public/**/*.{html,js}",
     "./public/**/*.{html,js}",
     "./Templates/**/*.{html,js}"],
