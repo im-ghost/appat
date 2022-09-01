cf19d288372a5ba50e529f4f0024cb9769dc4145
me <culestfrosh@gmail.com>
2022-09-01 20:58:13 +0100

    user model got updated

--- models/User.ts
+++ models/User.ts
@@ -8,7 +8,23 @@ import {
 } from '@interfaces/USER';
 
 const userModel: Schema = new Schema({
+  
   name: {
+    type: String
+  },
+  displayName: {
+    type: String
+  },
+  firstName: {
+    type: String
+  },
+  lastName: {
+    type: String
+  },
+  image: {
+    type: String
+  },
+  googleId: {
     type: String
   },
   email: {
