f61dbb008a415b73917c11e6717516289dd799ce
me <culestfrosh@gmail.com>
2022-09-03 14:23:31 +0100

    Removed process.exit

--- config/db.js
+++ config/db.js
@@ -10,7 +10,7 @@ const connectDB = async () => {
     console.log(`MongoDB Connected: ${conn.connection.host}`)
   } catch (err) {
     console.error(err)
-    process.exit(1)
+    //process.exit(1)
   }
 }
 
