fd94c0a589882d9a484c3d2b7d0936c8d9b58db7
me <culestfrosh@gmail.com>
2022-09-03 14:36:19 +0100

    deleted:

--- public/py/fake.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-url = '/fakedata'
-headers = {'Accept' : 'application/json', 'Content-Type' : 'application/json'}
-r = requests.post(url, data=open('example.json', 'rb'), headers=headers)
