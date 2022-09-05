dbf78f7388116cb00dcb1d691d46afc6b9289d99
me <culestfrosh@gmail.com>
2022-09-05 17:21:20 +0100

    -remoed alpine&:

--- public/javascripts/pages/room.js
+++ public/javascripts/pages/room.js
@@ -1,27 +1,34 @@
 const btn = document.querySelector("#send")
+/*
+class ObjectId {
+  constructor(id) {
+    this.id = id
+  }
+  value() {
+    return this.id
+  }
+}*/
 const {
   value
 } = document.querySelector("#message")
 const {
+  value: room
+} = document.querySelector("#room")
+const {
   value: user
 } = document.querySelector("#user")
+console.log(room)
+console.log(user)
 const send = (e)=> {
   const msg = {
     text: value,
     send: user,
     time: new Date()
   }
-  socket.emit("send message", msg)
+  socket.emit("send message", msg, room)
+  console.log(e)
 }
 btn.addEventListener("click", send)
-
-Alphine.data("roompage",({
-  desc:false,
-  main:true,
-  toggle(){
-    this.main = !this.main
-  }
-}))
 
 
 
