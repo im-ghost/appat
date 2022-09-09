eeda7f592bf8e303b5afbabac415885ee2cc1ea2
me <culestfrosh@gmail.com>
2022-09-09 09:19:00 +0100

    Folder for socket**

---
+++ socket/room.js
@@ -0,0 +1,25 @@
+//
+var Room = require("../models/Room");
+var User = require("../models/User");
+
+
+
+
+
+module.exports = (socket, io)=> {
+  socket.on("leave room", (user, room)=> {})
+  socket.on("new user", (user, room)=> {})
+  socket.on("send message", async (room)=> {
+    const update = {
+      messages: room.messages
+    }
+    const filter = {
+      _id: room._id
+    }
+    let doc = await Room.findOneAndUpdate(filter, update, {
+      returnOriginal: false,
+      new: true
+    });
+    console.log(doc)
+  })
+}
