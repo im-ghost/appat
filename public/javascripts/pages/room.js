07cd1ed2a73df0091cc80b334e46a694737dd833
me <culestfrosh@gmail.com>
2022-09-10 10:12:04 +0100

    :Life messages list update

--- public/javascripts/pages/room.js
+++ public/javascripts/pages/room.js
@@ -1,32 +1,98 @@
-(()=>{
+(()=> {
   const btn = document.querySelector("#send")
-let {
-  value: room
-} = document.querySelector("#room")
-let {
-  value: user
-} = document.querySelector("#user")
-room = JSON.parse(room)
-user = JSON.parse(user)
-console.log(room)
-console.log(user)
-const send = (e)=> {
-  
-const {
-  value
-} = document.querySelector("#message")
-  const msg = {
-    text: value,
-    sender: user,
-    time: new Date()
+  let {
+    value: room
+  } = document.querySelector("#room")
+  let {
+    value: user
+  } = document.querySelector("#user")
+  room = JSON.parse(room)
+  user = JSON.parse(user)
+  const send = (e)=> {
+
+    const {
+      value
+    } = document.querySelector("#message")
+    const msg = {
+      text: value,
+      sender: user,
+      time: new Date()
+    }
+    let all = room.messages
+    all.push(msg)
+    socket.emit("send message", room)
+
+    let df = document.querySelector("#room-msgs")
+    df.messages = all;
+    df.setAttribute("messages", JSON.stringify(all))
+    df.e = "jss"
+    console.log(df)
   }
-   room.messages.push(msg)
-  socket.emit("send message",room)
-  console.log(e)
-}
-btn.addEventListener("click", send)
+  btn.addEventListener("click", send)
 
 
 
-//
+  //
 })();
+
+const template = document.createElement("template")
+template.innerHTML = `
+
+<link rel="stylesheet" href="/stylesheets/output.css"/>
+<div id="list"></div>
+`
+
+
+class MsgList extends HTMLElement {
+  static get observedAttributes() {
+    return ["messages"]
+  }
+  constructor() {
+    super();
+    this.attachShadow({
+      mode: "open"
+    });
+    this.shadowRoot.appendChild(template.content.cloneNode(true))
+
+    const dom = this.shadowRoot;
+    const sel = (el) => {
+      return dom.querySelector(el)
+    }
+  }
+  get messages() {
+    return JSON.parse(this.getAttribute("messages"))
+
+  }
+  render() {
+    const dom = this.shadowRoot;
+    const sel = (el) => {
+      return dom.querySelector(el)
+    }
+    console.log("rendered ")
+    sel("#list").innerHTML = ""
+    this.messages.map((msg)=> {
+
+      let div = document.createElement("div")
+      div.innerHTML = `
+      <h1>${msg.text}</h1>
+      `
+      sel("#list").appendChild(div)
+    })
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+  disconnectedCallback() {}
+  attributeChangedCallback(name, oldValue, newValue) {
+    this.render();
+  }
+}
+
+
+const el = window.customElements.get("room-msgs")
+if (el) {
+  console.log(el)
+} else {
+  window.customElements.define("room-msgs", MsgList);
+}
