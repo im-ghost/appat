4fc15e95650fad8f684b691ff3b721a767c548ce
me <culestfrosh@gmail.com>
2022-09-13 22:57:13 +0100

    :Added theme toggler

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,7 +3,29 @@
 var socket = io();
 
 window.socket = socket;
-socket.emit("allRooms")/* += item;
+socket.emit("allRooms")
+//Them
+let theme;
+
+theme = localStorage.getItem("appat-theme") ||
+!!window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+? 'dark': 'light'
+
+document.documentElement.classList.add(theme)
+
+console.log(theme)
+let toggler = document.querySelector("#theme")
+toggler.addEventListener("click", ()=> {
+  
+    document.documentElement.classList.remove(theme)
+  
+  theme = theme === "dark" ? "light" :"dark";
+    localStorage.setItem("appat-theme", theme)
+document.documentElement.classList.add(theme)
+
+  console.log(theme)
+})
+/* += item;
   window.scrollTo(0, document.body.scrollHeight);
 });
 
