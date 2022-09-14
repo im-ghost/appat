0af437869316d12cc464176d8421f52cc7e9886d
me <culestfrosh@gmail.com>
2022-09-14 09:13:08 +0100

    memory:

---
+++ interfaces/memory.ts
@@ -0,0 +1,15 @@
+//
+//
+import USER from "./USER"
+export interface MEMORY {
+  _id: any,
+  user:String,
+  title:String,
+  body:String,
+  reactions:Array
+}
+
+export type MEMORIES = MEMORY[]
+//
+//
+//,
