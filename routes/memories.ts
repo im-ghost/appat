aa6ca604d3a95adaaeaacb1684c3e234180ef828
me <culestfrosh@gmail.com>
2022-09-15 16:08:16 +0100

    Sending full user object with the memories

--- routes/memories.ts
+++ routes/memories.ts
@@ -13,27 +13,28 @@ import {
   Response,
   NextFunction
 } from "express"
-const Memory = require("../models/memories")
-router.get("/", ensureAuth, (req: Request, res: Response)=> {
-  res.render("memories",{
-    title:"Memories",
-    user:req.user,
-  })
-})
+const Memory = require("../models/memory")
+const {
+  allMemories,
+  addMemory
+} = require ("../controllers/memory")
+router.get("/", ensureAuth, allMemories)
 
-router.get("/:id", ensureAuth,async (req: Request, res: Response)=> {
-  
+router.post("/addMemory", ensureAuth, addMemory)
+router.get("/addMemory", ensureAuth, (req: Request, res: Response)=>res.render("addMemory"))
+router.get("/memory/:id", ensureAuth, async (req: any, res: Response)=> {
+
   const memoryId = req.params.id;
-  const memory = await Memyory.findById(memoryId);
-  if(memory){
-  res.render("memory",{
-    memory:memory,
-    title:memory.title,
-    user:req.user
-  })
-  }
-  else{
+  const memory = await Memory.findById(memoryId);
+  if (memory) {
+    res.render("memory", {
+      memory: memory,
+      title: memory.title,
+      user: req.user
+    })
+  } else {
     res.redirect("/memories")
+  }
 })
 
 
