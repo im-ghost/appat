2ee983dcf130e19eeba6b6120bf1a8a9d3881fc9
me <culestfrosh@gmail.com>
2022-09-19 13:01:42 +0100

    Memories

--- public/javascripts/pages/memories.js
+++ public/javascripts/pages/memories.js
@@ -5,68 +5,69 @@ const MemoriesPage = ()=> {
   const {
     value: user
   } = document.querySelector("#user");
-
+  console.log("do");
   const {
     value: memories
   } = document.querySelector("#memories");
+
+  const addMemory = ()=> {}
+  document.querySelector("#addm").addEventListener("click", ()=> {
+    console.log("addMemory");
+    window.location.replace('/memories/addMemory')
+
+  })
+
 }
-  const template = document.createElement("template")
+const template = document.createElement("template")
 
-  template.innerHTML = `
+template.innerHTML = `
 
-  <link rel="stylesheet" href="/stylesheets/output.css"/>
-  <div x-data="memorylist" class="rounded-3xl shadow-2xl w-full border-2 p-4 m-4 text-4xl">
-  <header class="flex">
-  <img :src="img" class="rounded-[50%] h-32 w-32"/>
-  <div class="">
-  <h1 x-text="name"></h1>
+<link rel="stylesheet" href="/stylesheets/output.css"/>
+<div x-data="memorylist" class="rounded-3xl shadow-2xl w-full border-2 p-4 m-4 text-4xl">
+<header class="flex">
+<img src="" class="rounded-[50%] h-32 w-32"/>
+<div class="">
+<h1 x-text="name"></h1>
 
-  <p x-text="date"></p>
-  </div>
-  </header>
-  <main>
-  <p x-text="body"></p>
-  </main>
-  <div class="flex justify-evenly">
-  <span id="like" class="rounded-lg shadow p-4 flex align-center text-center justify-center w-1/4">👍</span>
-  <span id="share" class="rounded-lg shadow p-4 flex align-center text-center justify-center w-1/4">Share</span>
-  <span id="comment" class="rounded-lg shadow p-4 flex align-center text-center justify-center w-1/4">💬</span>
-  </div>
+<p x-text="date"></p>
+</div>
+</header>
+<main>
+<p x-text="body"></p>
+</main>
+<div class="flex justify-evenly">
+<span id="like" class="rounded-lg shadow p-4 flex align-center text-center justify-center w-1/4">👍</span>
+<span id="share" class="rounded-lg shadow p-4 flex align-center text-center justify-center w-1/4">Share</span>
+<span id="comment" class="rounded-lg shadow p-4 flex align-center text-center justify-center w-1/4">💬</span>
+</div>
 
 
 
-  </div>
-  `
+</div>
+`
 
-  class List extends HTMLElement {
-    constructor() {
-      super()
+class List extends HTMLElement {
+  constructor() {
+    super()
 
-      this.attachShadow({
-        mode: "open"
-      });
-      this.shadowRoot.appendChild(template.content.cloneNode(true))
-      this.memory = JSON.parse(this.getAttribute("memory"))
-      const {
-        title,
-        body,
-        reactions,
-        user
-      } = this.memory
-      const dom = this.shadowRoot;
-      const sel = (el) => {
-        return dom.querySelector(el)
-      }
-
+    this.attachShadow({
+      mode: "open"
+    });
+    this.shadowRoot.appendChild(template.content.cloneNode(true))
+    this.memory = JSON.parse(this.getAttribute("memory"))
+    const {
+      title,
+      body,
+      reactions,
+      user
+    } = this.memory
+    const dom = this.shadowRoot;
+    const sel = (el) => {
+      return dom.querySelector(el)
     }
 
-    connectedCallback() {
-
-      const addRoom = ()=> {
-        window.location.replace('/memories/addMemory')
-      
-    }
-    document.querySelector("#addm").addEventListener("click", addMemory)
+  }
+  connectedCallback() {
 
     window.addEventListener("alpine:init", ()=> {
 
@@ -74,14 +75,14 @@ const MemoriesPage = ()=> {
         init() {
           console.log(this.memory)
         },
-        title: this.memory.title,
-        userId: this.memory.user,
-        body: this.memory.body,
-        date: this.memory.date,
-        id: this.memory._id,
-        user: this.memory.userObj,
-        img: this.memory.userObj.image,
-        name: this.memory.userObj.displayName
+        title: this.memory.title || "",
+        userId: this.memory.user || "",
+        body: this.memory.body || "",
+        date: this.memory.date || "",
+        id: this.memory._id || "",
+        user: this.memory.userObj || "",/*
+        img: this.memory.userObj.image || "",*/
+        name: this.memory.userObj.displayName || ""
       }))
 
 
@@ -105,4 +106,4 @@ if (el) {
 
 
 
-document.addEventListener("DOMContentLoaded", MemoriesPage)
+window.addEventListener("DOMContentLoaded", MemoriesPage)
