c1ce278b91caaafba08fd56f6ecae57510573a85
me <culestfrosh@gmail.com>
2022-09-22 10:12:22 +0100

    as rooms

--- routes/chats.ts
+++ routes/chats.ts
@@ -1,32 +1,59 @@
 const {
   Router
 } = require("express")
-
+var path = require("path");
+var Room = require("../models/Room")
 const {
   ensureAuth,
   ensureGuest
 } = require('../Middlewares/auth');
-
 
 import {
   Request,
   Response,
   NextFunction
 } from "express"
-
+import {
+  ROOM,
+  ROOMS
+} from "../interfaces/ROOM"
+import {
+  addRoom,
+  singleRoom
+} from "../controllers/room"
 const router = Router();
 
-router.get("/",ensureAuth, (req:Request, res:Response, next:NextFunction)=> {
-  res.render("chats", {
-    title: "All chats"
+router.get("/", ensureAuth, async (req: Request, res: Response)=>
+  {
+    let chats = await Room.find({})
+    if (chats) {
+      res.render("chats", {
+        title: "Chats",
+        chats: chats
+      })
+    } else {
+      console.log(chats)
+    }
   })
-})
-
-
-router.get("/:id",ensureAuth, (req:Request, res:Response, next:NextFunction)=> {
-  res.render("chat", {
-    title: `chat`
+router.get("/chat/:id", ensureAuth, singleRoom)
+router.post("/addRoom", ensureAuth, addRoom)
+router.get("/chat/:id", ensureAuth, singleRoom)
+router.get("/addRoom", ensureAuth, (req: any, res: Response)=>res.render("addRoom")
+router.get("/add", ensureAuth, async (req: any, res: Response)=> {
+  const friends = []
+  await req.user.friends.map(async(friend, i)=> {
+    const friendWithObj = await User.findById(friend)
+    friends.push(friendWithObj)
   })
-})
-
+  res.render("add", {
+    user: req.user,
+    friends: friends
+  })})
 module.exports = router
+
+//
+//*
+
+
+
+/**/
